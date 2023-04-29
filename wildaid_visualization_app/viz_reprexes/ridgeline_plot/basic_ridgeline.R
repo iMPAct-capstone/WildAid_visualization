6e6812504864f776fedd66476099cd3181b16d12
jaredbpetry <jaredpetry@gmail.com>
2023-04-29 12:49:02 -0700

    playing with the ridge plot... adding a faceted histogram too

---
+++ wildaid_visualization_app/viz_reprexes/ridgeline_plot/basic_ridgeline.R
@@ -0,0 +1,64 @@
+library(shiny)
+library(dplyr) 
+library(tidyverse) 
+library(leaflet) 
+library(plotly) 
+library(ggplot2) 
+library(googledrive) 
+library(googlesheets4)
+library(radiant.data)
+library(DT)
+library(janitor)
+library(shinycssloaders)
+library(hrbrthemes)
+library(shinyWidgets)
+library(lubridate)
+library(scales)
+library(ggridges)
+
+url <- "https://docs.google.com/spreadsheets/d/1cUz4WZ1CRHFicuVUt82kJ_mL9Ur861Dn1c0BYu3NmRY/edit#gid=0"
+
+# LEAVE THIS ALONE... MUST BE CONSISTENT... MAKE CHANGES IN REACTIVE DATAFRAME
+# get our MPS tracker data MAKE SURE THIS IS CONSISTENT IN GLOBAL.R FILE IN APP
+MPS_tracker_data <- read_sheet(url) |> 
+  mutate(year = as.factor(year),  # the year is getting super funky with decimals eww!
+         category = as.factor(category), 
+         country = as.factor(country)) |> 
+  select(-indicator_type)
+
+# add below to the app 
+
+ridgeplot <- MPS_tracker_data |> 
+  select(country, score) |> 
+  na.omit() |> 
+  ggplot(aes(x = score, y = country, fill = country)) +
+  stat_binline(scale = 0.9, stat = "binline", bins = 5, binwidth = 1, 
+                       draw_baseline = FALSE,
+                       quantile_lines=TRUE,
+                       quantiles = 2,
+                       quantile_fun=function(x,...)mean(x)) +
+  scale_x_continuous(breaks = c(1,2,3,4,5), limits = c(0,6)) +
+  theme(panel.spacing = unit(1, "lines"),  # i forgot what this line does 
+        legend.position = 'none', 
+        axis.title.y = element_blank(), 
+        axis.title.x = element_text(hjust = 0.5)) + 
+  labs(title = "Score distributions by country")
+
+
+ridgeplot
+
+# ok lets try a simple histogram faceted by country so we can do fancier stuff
+facet_hist <- MPS_tracker_data |> 
+  select(country, score) |> 
+  na.omit() |> 
+  ggplot(aes(x = score, fill = country)) + 
+  geom_histogram(aes(y = ..density..), 
+                 binwidth = 1, bins = 5) + 
+  geom_vline(aes(xintercept = mean(score))) +
+  facet_wrap(~country, ncol = 2, scales = 'free') + 
+  scale_x_continuous(breaks = c(1,2,3,4,5), limits = c(0,6)) +
+  scale_y_continuous(limits = c(0,0.6)) +
+  theme_bw() + 
+  theme(legend.position = "none") 
+
+facet_hist
